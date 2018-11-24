abb8507c3aacebc1ffe6d2be61b563d6f47e54dc
Muhammad Ridha Anshari <mridhaans@gmail.com>
2018-11-24 23:08:41 +0700

    fixing path error

--- movies_project/movies_project/settings.py
+++ movies_project/movies_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'movies_project/movies_api',
+    'movies_project',
     'django_filters',
 ]
 
