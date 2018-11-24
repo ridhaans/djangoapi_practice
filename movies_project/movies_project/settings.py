1fd4c7bbe641411187e3229bbef0ebcd404c03cd
Muhammad Ridha Anshari <mridhaans@gmail.com>
2018-11-24 17:22:27 +0700

    trying to deploy on heroku

--- movies_project/movies_project/settings.py
+++ movies_project/movies_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL='movies_api.UserProfile'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
