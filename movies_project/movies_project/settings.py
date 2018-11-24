bce5ab63d5d291117234cf22bf9c86cf5c3721e8
Muhammad Ridha Anshari <mridhaans@gmail.com>
2018-11-24 22:11:45 +0700

    adding Python version to requirements

--- movies_project/movies_project/settings.py
+++ movies_project/movies_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',    
+    'rest_framework.authtoken',
+    'movies_api',
     'django_filters',
 ]
 
